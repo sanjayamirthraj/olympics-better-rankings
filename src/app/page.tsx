e72a5babe6fb24b5ad8259e5009afeb76247662b
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-08-12 15:58:45 -0500

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,87 @@
-import Image from "next/image";
+import countries from '@/app/api/countries.json';
+import MenuBar from '@/components/ui/navigationbar';
+import { countryCodeMapping } from './api/countrymap';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import Image from 'next/image';
 
-export default function Home() {
+const GOLD = 1027
+const SILVER = 535
+const BRONZE = 4.60
+
+
+const fetchMedalData = async (countryCode: string | null) => {
+  const response = await fetch(`https://api.olympics.kevle.xyz/medals?country=${countryCode}`);
+  const data = await response.json();
+  return data; 
+};
+
+const medalDataArray = (await Promise.all(countries.map(async (country) => {
+  const medalData = await fetchMedalData(country.iso_alpha_3);
+  const gold = medalData?.results[0]?.medals?.gold || 0;
+  const silver = medalData?.results[0]?.medals?.silver || 0;
+  const bronze = medalData?.results[0]?.medals?.bronze || 0;
+  const totalValue = (gold * GOLD + silver * SILVER + bronze * BRONZE).toFixed(2);
+  const originalRank = medalData?.results[0]?.rank
+  return { country, totalValue, gold, silver, bronze, originalRank };
+}))).filter(data => data.country && data.gold > 0 || data.silver > 0 || data.bronze > 0);
+
+const sortedCountries = medalDataArray.sort((a, b) => parseFloat(b.totalValue) - parseFloat(a.totalValue));
+
+
+export default async function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='p-12'>
+      <div className='p-10'><MenuBar/></div>
+      <div className="text-center p-10 bg-white shadow-lg rounded-lg">
+      <h1 className="text-4xl font-bold mb-4">The Correct Olympic Medals Ranking</h1>
+      <p className="text-lg mb-8 text-center">Ranking Countries by Gold undermines the achievements of Silver and Bronze Medalists! But ranking total medals just does the opposite. Like true consumerists, let&#39;s rank each country by the total value of each medal!</p>
+
+        <div className="flex justify-center">
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b1/Olympic_Rings.svg/1600px-Olympic_Rings.svg.png?20111003031241" alt="" style={{ width: '25%', height: 'auto' }} />
+        </div>        
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='p-10 bg-slate-100	'>
+      <Table className='pt-10 '>
+        <TableCaption>Thanks for Visiting! Contact me @sanjayamirthraj</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Country</TableHead>
+            <TableHead>Flag</TableHead>
+            <TableHead>Gold</TableHead>
+            <TableHead>Silver</TableHead>
+            <TableHead>Bronze</TableHead>
+            <TableHead>Total Value</TableHead>
+            <TableHead>Original Rank</TableHead>
+            <TableHead className='text-right'>Value Rank</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {await Promise.all(sortedCountries.map(async (country) => {
+            return (
+              <TableRow key={country.country.iso_alpha_3}>
+                <TableCell className="font-medium">{country.country.iso_alpha_3}</TableCell>
+                <TableCell><img src={`https://flagsapi.com/${countryCodeMapping[country.country.iso_alpha_3 as keyof typeof countryCodeMapping] || "US"}/flat/32`} alt={country.country.iso_alpha_3 || "flag"} /></TableCell>
+                <TableCell>{country.gold}</TableCell>
+                <TableCell>{country.silver}</TableCell>
+                <TableCell>{country.bronze}</TableCell>
+                <TableCell>{'$' + country.totalValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</TableCell>
+                <TableCell>{country.originalRank}</TableCell>
+                <TableCell className="text-right">{sortedCountries.indexOf(country) + 1}</TableCell>
+              </TableRow>
+            );
+          }))}
+        </TableBody>
+      </Table>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ src/components/ui/navigationbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { JSX, SVGProps } from "react";
+
+export default function MenuBar() {
+  return (
+    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
+      <div className="w-full max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-14 items-center">
+          <Link className="flex items-center" href="/">
+            The Paris 2024 Olympic Medals Value ranking!
+          </Link>
+          <nav className="hidden md:flex gap-4">
+          
+            <Link
+              className="font-medium flex items-center text-sm transition-colors hover:underline"
+              href="https://www.deseret.com/u-s-world/2024/08/07/how-much-gold-olympic-medal-value/"
+            >
+              Where did I get these numbers from?
+            </Link>
+         
+          </nav>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function MountainIcon(
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  );
+}
